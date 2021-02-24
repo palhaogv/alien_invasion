bee623cf902a756906a0841dd35d31d3512bed35
palhaogv <palhaogv@gmail.com>
2021-02-24 18:30:57 +0000

    endday

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (53, 81, 92)
+        self.bg_color = (135, 206, 235)
